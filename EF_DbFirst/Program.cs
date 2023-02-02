a7716bc2fced688c6579a76778407511321bd3a8
Huseyn Hamidov <88426345+hhuseyn1@users.noreply.github.com>
2023-02-02 18:11:17 +0400

    Related Data Load

--- EF_DbFirst/Program.cs
+++ EF_DbFirst/Program.cs
@@ -1,7 +1,73 @@
 ﻿using EF_DbFirst.DAL;
 using EF_DbFirst.Models;
+using Microsoft.EntityFrameworkCore;
 
 using var context = new AppDbContext();
+using var libContext = new LibraryContext ();
+
+
+
+//Related Data Load
+
+//1)Eager loading
+//2)Explicit loading
+//3)Lazy loading
+
+//1)Eager loading
+//var authors = libContext.Authors.Include(a => a.Books).ThenInclude(b => b.IdCategoryNavigation)
+//    .Include(a => a.Books).ThenInclude(b => b.IdThemesNavigation).ToList();
+
+
+//foreach (var p in authors)
+//{
+//    Console.WriteLine($"{p.Id} {p.FirstName} {p.LastName}");
+//    foreach (var book in p.Books)
+//    {
+//        Console.WriteLine($"{book.Id} {book.Name} {book.Comment}");
+
+//    }
+//}
+
+
+
+//2)Explicit loading
+
+//var authors = libContext.Authors.Include(a => a.Books).ToList();
+
+
+//foreach (var p in authors)
+//{
+//    Console.WriteLine($"{p.Id} {p.FirstName} {p.LastName}");
+//    if (true)
+//    {
+//        libContext.Entry(p).Collection(x => x.Books).Load();
+//        foreach (var book in p.Books)
+//        {
+//            Console.WriteLine($"{book.Id} {book.Name} {book.Comment}");
+
+//        }
+//    }
+//}
+
+
+//Add Nuget package Proxies
+//3)Lazy loading
+
+var authors = libContext.Authors.ToList();
+
+
+foreach (var author in authors)
+{
+    foreach (var book in author.Books)
+    {
+        Console.WriteLine(book);
+    }
+    Console.WriteLine(author.Books.Count);
+}
+
+
+
+
 
 //foreach (var p in context.Products)
 //{
@@ -9,10 +75,9 @@ using var context = new AppDbContext();
 
 //}
 
-using var libContext = new LibraryContext ();
 
-foreach (var p in libContext.Books)
-{
-    Console.WriteLine($"{p.Id} {p.Name} {p.YearPress}");
+//foreach (var p in libContext.Books)
+//{
+//    Console.WriteLine($"{p.Id} {p.Name} {p.YearPress}");
 
-}
+//}
