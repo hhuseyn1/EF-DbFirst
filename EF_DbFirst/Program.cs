cdc3a14d12be455be5ea821abc5500c972b9e2ac
Huseyn Hamidov <88426345+hhuseyn1@users.noreply.github.com>
2023-02-02 12:56:03 +0400

    Read Lib data

--- EF_DbFirst/Program.cs
+++ EF_DbFirst/Program.cs
@@ -1,9 +1,18 @@
 ﻿using EF_DbFirst.DAL;
+using EF_DbFirst.Models;
 
 using var context = new AppDbContext();
 
-foreach (var p in context.Products)
+//foreach (var p in context.Products)
+//{
+//    Console.WriteLine($"{p.Id} {p.Name} {p.UnitPrice} {p.Description}");
+
+//}
+
+using var libContext = new LibraryContext ();
+
+foreach (var p in libContext.Books)
 {
-    Console.WriteLine($"{p.Id} {p.Name} {p.UnitPrice} {p.Description}");
+    Console.WriteLine($"{p.Id} {p.Name} {p.YearPress}");
 
 }
