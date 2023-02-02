95d8c07775421585ddf35fee293362fa2a2fb15b
Huseyn Hamidov <88426345+hhuseyn1@users.noreply.github.com>
2023-02-02 12:50:32 +0400

    Scaffold

---
+++ EF_DbFirst/Models/Faculty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EF_DbFirst.Models
+{
+    public partial class Faculty
+    {
+        public Faculty()
+        {
+            Groups = new HashSet<Group>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<Group> Groups { get; set; }
+    }
+}
---
+++ EF_DbFirst/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EF_DbFirst.Models
+{
+    public partial class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal UnitPrice { get; set; }
+        public string? Description { get; set; }
+    }
+}
